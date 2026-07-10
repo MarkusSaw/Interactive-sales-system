5c67c7cd663741628221db26f2c47545b3eeac72
MarkusSaw <vadirai@yandex.ru>
2026-07-10 17:18:03 +0300

    feature(Interactive-sales-system): Корректировка класса юнит тест

--- Interactive-sales-system/src/test/java/Interactive/sales/system/tests/unit/DiscountCalculateTest.java
+++ Interactive-sales-system/src/test/java/Interactive/sales/system/tests/unit/DiscountCalculateTest.java
@@ -1,9 +1,10 @@
+package Interactive.sales.system.tests.unit;
+
 import Interactive.sales.system.discountservice.DiscountCalculate;
 import Interactive.sales.system.dto.DtoOrder;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,11 +19,11 @@ public class DiscountCalculateTest {
 
         DtoOrder order = new DtoOrder(LocalDateTime.now(), "Broke Company", 1000);
 
-        List<DtoOrder> orders = Arrays.asList(order);
+        List<DtoOrder> orders = List.of(order);
 
         Map<String, Double> result = discountCalculate.calculate(orders, 10.0, 50.0, 5.0);
 
-        assertEquals(5000.0, result.get("Broke Company"), 0.01);
+        assertEquals(5000.0, result.get("Broke Company"));
     }
 
 
