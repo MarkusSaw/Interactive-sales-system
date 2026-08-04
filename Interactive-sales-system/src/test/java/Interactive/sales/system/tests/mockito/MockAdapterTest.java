904862c2c8ff3538fabfb61f943301a43b2d8b6f
MarkusSaw <vadirai@yandex.ru>
2026-08-04 23:44:49 +0300

    feature(Interactive-sales-system): написал моск тест для адатера и калькулятора.

--- Interactive-sales-system/src/test/java/Interactive/sales/system/tests/mockito/MockAdapterTest.java
+++ Interactive-sales-system/src/test/java/Interactive/sales/system/tests/mockito/MockAdapterTest.java
@@ -3,40 +3,33 @@ package Interactive.sales.system.tests.mockito;
 
 import Interactive.sales.system.adapter.OrderAdapter;
 import Interactive.sales.system.adapter.OrderAdapterFactory;
-import Interactive.sales.system.dto.DtoOrder;
+import Interactive.sales.system.adapter.TxtOrderAdapter;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MockAdapterTest {
 
-    private final OrderAdapter noworder;
 
-    public MockAdapterTest(OrderAdapter noworder) {
-        this.noworder = noworder;
-    }
-
-@Mock
-OrderAdapter noworder;
-
-    @InjectMocks
+    @Mock
     OrderAdapterFactory nowpasteadapter;
 
-@Test
-    void adaptGetAdapter(){
-    when(noworder.adapt()).thenReturn();
-
-    List<DtoOrder> ok = nowpasteadapter.getAdapter();
-    assertTrue(ok);
-    verify(noworder).adapt();
-}
+    @Test
+    void adaptGetAdapter() {
+        TxtOrderAdapter adapter = mock(TxtOrderAdapter.class);
+        when(nowpasteadapter.getAdapter(anyString())).thenReturn(adapter);
+        OrderAdapter result = nowpasteadapter.getAdapter("test.txt");
+        assertEquals(adapter, result);
+        verify(nowpasteadapter).getAdapter("test.txt");
+    }
 }
